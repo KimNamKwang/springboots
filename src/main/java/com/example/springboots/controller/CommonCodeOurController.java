493a8682a1e2f88a9fddc80c7980adb415d46d75
KimNamKwang <durante0916@gmail.com>
2023-01-25 14:38:14 +0900

    jkl

--- src/main/java/com/example/springboots/controller/CommonCodeOurController.java
+++ src/main/java/com/example/springboots/controller/CommonCodeOurController.java
@@ -1,5 +1,9 @@
 package com.example.springboots.controller;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.springboots.service.CommonCodeOurService;
@@ -22,8 +28,19 @@ public class CommonCodeOurController {
     CommonCodeOurService commonCodeOurService;
 
     @RequestMapping(value = { "/insert" }, method = RequestMethod.POST)
-    public ModelAndView insert(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
+    public ModelAndView insert(MultipartHttpServletRequest multipartHttpServletRequest,
+            @RequestParam Map<String, Object> params, ModelAndView modelAndView) throws IOException {
+
+        String registerSeq = multipartHttpServletRequest.getParameter("REGISTER_SEQ");
+        MultipartFile multipartFile = multipartHttpServletRequest.getFile("file_first");
+        String fileName = multipartFile.getOriginalFilename();
+
+        String relativePath = "src\\main\\resources\\static\\files\\";
+        // file 저장
+        BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(relativePath + fileName));
+        bufferedWriter.write(new String(multipartFile.getBytes()));
         Object resultMap = commonCodeOurService.insertOneAndGetList(params);
+
         modelAndView.addObject("resultMap", resultMap);
         modelAndView.setViewName("commonCode_our/list");
         return modelAndView;
@@ -45,7 +62,7 @@ public class CommonCodeOurController {
         return modelAndView;
     }
 
-    @RequestMapping(value = { "/deleteMulti" }, method = RequestMethod.GET)
+    @RequestMapping(value = { "/deleteMulti" }, method = RequestMethod.POST)
     public ModelAndView deleteMulti(HttpServletRequest httpServletRequest, @RequestParam Map<String, Object> params,
             ModelAndView modelAndView) {
         // modelAndView.addObject("resultMap", resultMap);
