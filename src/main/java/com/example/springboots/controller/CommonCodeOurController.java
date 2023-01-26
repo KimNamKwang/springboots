c787a82b1fc40d98cd458306b50d0b4e6bb09cc4
KimNamKwang <durante0916@gmail.com>
2023-01-26 13:38:39 +0900

    파일 올리기

--- src/main/java/com/example/springboots/controller/CommonCodeOurController.java
+++ src/main/java/com/example/springboots/controller/CommonCodeOurController.java
@@ -1,9 +1,14 @@
 package com.example.springboots.controller;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +24,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.springboots.service.CommonCodeOurService;
+import com.example.springboots.utils.CommonUtils;
 
 @Controller
 @RequestMapping(value = "/commonCodeOur")
@@ -26,6 +32,9 @@ public class CommonCodeOurController {
 
     @Autowired
     CommonCodeOurService commonCodeOurService;
+
+    @Autowired
+    CommonUtils commonUtils;
 
     @RequestMapping(value = { "/insert" }, method = RequestMethod.POST)
     public ModelAndView insert(MultipartHttpServletRequest multipartHttpServletRequest,
@@ -46,9 +55,52 @@ public class CommonCodeOurController {
         return modelAndView;
     }
 
+    @RequestMapping(value = { "/insertMulti" }, method = RequestMethod.POST)
+    public ModelAndView insertMulti(MultipartHttpServletRequest multipartHttpServletRequest,
+            @RequestParam Map<String, Object> params, ModelAndView modelAndView) throws IOException {
+
+        Iterator<String> fileNames = multipartHttpServletRequest.getFileNames();
+        String relativePath = "C:\\Develops\\springboots\\src\\main\\resources\\static\\files\\";
+
+        Map attachFile = null;
+        List attachFiles = new ArrayList<>();
+        String physicalFileName = commonUtils.getUniqueSequence();
+        String storePath = relativePath + physicalFileName + "\\";
+        File newPath = new File(storePath);
+        newPath.mkdir(); // 폴더 만들어줌
+        while (fileNames.hasNext()) {
+            String fileName = fileNames.next();
+            MultipartFile multipartFile = multipartHttpServletRequest.getFile(fileName);
+            String originalFileName = multipartFile.getOriginalFilename();
+            String storePathFileName = storePath + originalFileName;
+            multipartFile.transferTo(new File(storePathFileName));
+            // SOURCE_UNIQUE_SEQ(params로 사용가능), ORGINALFILE_NAME, PHYSICALFILE_NAME
+            attachFile = new HashMap<>();
+            attachFile.put("SOURCE_UNIQUE_SEQ", params.get("COMMON_CODE_ID"));
+            attachFile.put("ORGINALFILE_NAME", originalFileName);
+            attachFile.put("PHYSICALFILE_NAME", physicalFileName);
+
+            attachFiles.add(attachFile);
+        }
+
+        params.put("attachFiles", attachFiles);
+
+        Object resultMap = commonCodeOurService.insertWithFilesAndGetList(params);
+
+        modelAndView.addObject("resultMap", resultMap);
+        modelAndView.setViewName("commonCode_our/list");
+        return modelAndView;
+    }
+
     @RequestMapping(value = { "/form" }, method = RequestMethod.GET)
     public ModelAndView form(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
         modelAndView.setViewName("commonCode_our/edit");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = { "/formMulti" }, method = RequestMethod.GET)
+    public ModelAndView formMulti(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
+        modelAndView.setViewName("commonCode_our/editMulti");
         return modelAndView;
     }
 
